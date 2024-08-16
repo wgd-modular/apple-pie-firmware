8a85b583c601cd851efa76d6abd0115394b1ba77
Felix Wiegand <fxwiegand@gmail.com>
2024-08-16 22:02:40 +0200

    Add first workin code

--- apple-pie-firmware.ino
+++ apple-pie-firmware.ino
@@ -1,17 +1,32 @@
 #include <MCP48xx.h>
-#include <avr/interrupt.h>
 
-MCP4822 dac(10);
-
+// Pin Definitions
 const int GATE_OUT1 = 2;
 const int GATE_OUT2 = 3;
 const int CLOCK_IN = 9;
+const int LOCK_PIN = A4;
 
-volatile bool clockState = LOW;
-volatile int clockCount = 0;
+// State Variable for Clock
+volatile bool clockState1 = LOW;
+volatile bool clockState2 = LOW;
+
+
+// Shift Register Arrays
+uint16_t shiftRegister1 = 0x0000; // Example initial value
+uint16_t shiftRegister2 = 0x0000; // Example initial value
+
+// Function to Read Analog Input
+uint16_t readAnalogInput(uint8_t pin) {
+    return analogRead(pin);
+}
+
+MCP4822 dac(10);
 
 void setup() {
-    Serial.begin(9600);
+    // Initialize Pin Modes
+    pinMode(GATE_OUT1, OUTPUT);
+    pinMode(GATE_OUT2, OUTPUT);
+    pinMode(CLOCK_IN, INPUT);
 
     dac.init();
     dac.turnOnChannelA();
@@ -20,37 +35,28 @@ void setup() {
     dac.setGainB(MCP4822::High);
 
     randomSeed(analogRead(7));
-
-    pinMode(GATE_OUT1, OUTPUT);
-    pinMode(GATE_OUT2, OUTPUT);
-
-    pinMode(CLOCK_IN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(CLOCK_IN), handleClock, RISING);
 }
 
 void loop() {
-    digitalWrite(GATE_OUT1, clockState);
+    // Read the clock input state
+    bool currentClockState = digitalRead(CLOCK_IN);
+    int lockValue = readAnalogInput(LOCK_PIN);
+    
 
-    if (clockCount % 2 == 0) {
-        digitalWrite(GATE_OUT2, clockState);
+    // Detect rising edge
+    if (currentClockState == HIGH && clockState1 == LOW) {
+        clockState1 = HIGH;
+        dac.setVoltageA(shiftRegister1 >> 4);
+        dac.setVoltageB(shiftRegister2 >> 4);
+        dac.updateDAC();
+        digitalWrite(GATE_OUT1, shiftRegister1 >= 0x8000);
+        shiftRegister1 = (shiftRegister1 << 1) | (lockValue < random(0,2048) ? (shiftRegister1 >> 15) : (~shiftRegister1 >> 15));
+        digitalWrite(GATE_OUT2, shiftRegister2 >= 0x8000);
+        shiftRegister2 = (shiftRegister2 << 1) | (lockValue < random(0,2048) ? (shiftRegister2 >> 15) : (~shiftRegister2 >> 15));
+    } else if (currentClockState == LOW) {
+        // Reset the state when the clock goes low
+        clockState1 = LOW;
+        digitalWrite(GATE_OUT1, LOW);
+        digitalWrite(GATE_OUT2, LOW); 
     }
-
-    int voltageA = random(0, 4096);
-    int voltageB = random(0, 4096);
-
-    dac.setVoltageA(voltageA);
-    dac.setVoltageB(voltageB);
-
-    dac.updateDAC();
-
-    delay(10);
-}
-
-void handleClock() {
-    clockState = !clockState;
-    clockCount++;
-}
-
-uint16_t readAnalogInput(uint8_t pin) {
-    return analogRead(pin);
 }
