4889725c74d3a09afb93a84077e21476613c280a
erebusnz <stig@sdm.co.nz>
2024-09-28 22:39:22 -0700

    Update apple-pie-firmware.ino
    Refactor for interrupt using clock pin

--- apple-pie-firmware.ino
+++ apple-pie-firmware.ino
@@ -3,33 +3,37 @@
 // Pin Definitions
 const int GATE_OUT1 = 2;
 const int GATE_OUT2 = 3;
-const int CLOCK_IN = 9;
+const int XTAL = 7;
+const int CLOCK_PIN = 9;
 const int LOCK_PIN = A4;
 const int CV_1 = A0;
 const int CV_2 = A1;
+const int STEPS_PIN = A2;
+const int SWITCH_PIN = A3;
+const int DAC_PIN = 10;
 
-// State Variable for Clock
-volatile bool clockState1 = LOW;
-volatile bool clockState2 = LOW;
+MCP4822 dac(DAC_PIN);
 
+// Global Setting Values
+volatile uint16_t lockValueA = 0;
+volatile uint16_t lockValueB = 0;
+volatile uint8_t shiftRegisterLength = 16;
+volatile bool switchPosition = true;
 
 // Shift Register Arrays
-uint16_t shiftRegister1 = 0x0000; // Example initial value
-uint16_t shiftRegister2 = 0x0000; // Example initial value
+volatile uint16_t shiftRegister1 = 0x0000;
+volatile uint16_t shiftRegister2 = 0x0000;
+uint16_t last_cv_1 = -1;
+uint16_t last_cv_2 = -1;
 
-// Function to Read Analog Input
-uint16_t readAnalogInput(uint8_t pin) {
-    return analogRead(pin);
-}
-
-// Function to Read Analog Input
+// Function to read switch position
 bool readSwitchPosition() {
-    return analogRead(A3) > 512;
+    return analogRead(SWITCH_PIN) > 512;
 }
 
 uint8_t getShiftRegisterLength() {
-    // Read the analog value from pin A2 (range 0 to 1023)
-    uint16_t analogValue = analogRead(A2);
+    // Read the analog value from STEPS_PIN (range 0 to 1023)
+    uint16_t analogValue = analogRead(STEPS_PIN);
 
     // Map the analog value to one of the specific return values: 2, 4, 8, or 16
     if (analogValue < 256) {
@@ -54,14 +58,30 @@ uint16_t clearNthLeftBit(uint16_t value, uint8_t n) {
     return value & mask;
 }
 
-
-MCP4822 dac(10);
+void doClockCycle() {
+  // Rising edge
+  if (!digitalRead(CLOCK_PIN)) {
+        
+        digitalWrite(GATE_OUT1, shiftRegister1 >= 0x8000);
+        shiftRegister1 = clearNthLeftBit((shiftRegister1 << 1), shiftRegisterLength) | (lockValueA < random(0,2048) ? (shiftRegister1 >> (shiftRegisterLength - 1)) : (~shiftRegister1 >> (shiftRegisterLength - 1)));
+        digitalWrite(GATE_OUT2, shiftRegister2 >= 0x8000);
+        // Invert functionality of lockValue when switch is in reverse position
+        if (switchPosition) {
+          shiftRegister2 = clearNthLeftBit((shiftRegister2 << 1), shiftRegisterLength) | (lockValueB < random(0,2048) ? (shiftRegister2 >> (shiftRegisterLength - 1)) : (~shiftRegister2 >> (shiftRegisterLength - 1)));
+        } else {
+          shiftRegister2 = clearNthLeftBit((shiftRegister2 << 1), shiftRegisterLength) | ((1024 - lockValueB) < random(0,2048) ? (shiftRegister2 >> (shiftRegisterLength - 1)) : (~shiftRegister2 >> (shiftRegisterLength - 1)));
+        }
+    } else {
+        digitalWrite(GATE_OUT1, LOW);
+        digitalWrite(GATE_OUT2, LOW); 
+    }
+}
 
 void setup() {
     // Initialize Pin Modes
     pinMode(GATE_OUT1, OUTPUT);
     pinMode(GATE_OUT2, OUTPUT);
-    pinMode(CLOCK_IN, INPUT);
+    pinMode(CLOCK_PIN, INPUT);
 
     dac.init();
     dac.turnOnChannelA();
@@ -69,40 +89,35 @@ void setup() {
     dac.setGainA(MCP4822::High);
     dac.setGainB(MCP4822::High);
 
-    randomSeed(analogRead(7));
+  randomSeed(analogRead(XTAL));
+
+  // set up Arduino interrupt
+  attachInterrupt(digitalPinToInterrupt(CLOCK_PIN), doClockCycle, CHANGE);
 }
 
 void loop() {
-    // Read the clock input state
-    bool currentClockState = digitalRead(CLOCK_IN);
-    int lockValue = readAnalogInput(LOCK_PIN);
-    int cvA = readAnalogInput(CV_1) * 1.7;
-    int cvB = readAnalogInput(CV_2) * 1.7;
-    uint16_t lockValueA = (uint16_t)(constrain((int) lockValue + (int) cvA - 525, 0, 1023));
-    uint16_t lockValueB = (uint16_t)(constrain((int) lockValue + (int) cvB - 525, 0, 1023));
-    uint8_t shiftRegisterLength = getShiftRegisterLength();
-    
-    // Detect rising edge
-    if (currentClockState == HIGH && clockState1 == LOW) {
-        clockState1 = HIGH;
-        
-        dac.setVoltageA(shiftRegister1 >> 4);
-        dac.setVoltageB(shiftRegister2 >> 4);
-        dac.updateDAC();
-        
-        digitalWrite(GATE_OUT1, shiftRegister1 >= 0x8000);
-        shiftRegister1 = clearNthLeftBit((shiftRegister1 << 1), shiftRegisterLength) | (lockValueA < random(0,2048) ? (shiftRegister1 >> (shiftRegisterLength - 1)) : (~shiftRegister1 >> (shiftRegisterLength - 1)));
-        digitalWrite(GATE_OUT2, shiftRegister2 >= 0x8000);
-        // Invert functionality of lockValue when switch is in reverse position
-        if (readSwitchPosition()) {
-          shiftRegister2 = clearNthLeftBit((shiftRegister2 << 1), shiftRegisterLength) | (lockValueB < random(0,2048) ? (shiftRegister2 >> (shiftRegisterLength - 1)) : (~shiftRegister2 >> (shiftRegisterLength - 1)));
-        } else {
-          shiftRegister2 = clearNthLeftBit((shiftRegister2 << 1), shiftRegisterLength) | ((1024 - lockValueB) < random(0,2048) ? (shiftRegister2 >> (shiftRegisterLength - 1)) : (~shiftRegister2 >> (shiftRegisterLength - 1)));
-        }
-    } else if (currentClockState == LOW) {
-        // Reset the state when the clock goes low
-        clockState1 = LOW;
-        digitalWrite(GATE_OUT1, LOW);
-        digitalWrite(GATE_OUT2, LOW); 
+    int lockValue = analogRead(LOCK_PIN);
+    int cvA = analogRead(CV_1) * 1.7;
+    int cvB = analogRead(CV_2) * 1.7;
+
+    //Set lock values
+    lockValueA = (uint16_t)(constrain((int) lockValue + (int) cvA - 525, 0, 1023));
+    lockValueB = (uint16_t)(constrain((int) lockValue + (int) cvB - 525, 0, 1023));
+
+    //Length of sequence (2,4,8,16) selected by STEPS_PIN
+    shiftRegisterLength = getShiftRegisterLength();
+    switchPosition = readSwitchPosition();
+
+    //Send new CV if shiftRegisters are changed
+    uint16_t new_cv_1 = shiftRegister1 >> 4;
+    uint16_t new_cv_2 = shiftRegister2 >> 4;
+    if (last_cv_1 != new_cv_1) {
+      last_cv_1 = new_cv_1;
+      dac.setVoltageA(last_cv_1);
     }
+    if (last_cv_2 != new_cv_2) {
+      last_cv_2 = new_cv_2;
+      dac.setVoltageB(new_cv_2);
+    }
+    dac.updateDAC();
 }
